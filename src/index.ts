1b1cce0a421cd8c246f41b8571829fc1781f4894
Agbele Kolade <kolagbele@gmail.com>
2021-11-16 15:35:24 +0100

    eslint configurd

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,20 @@ import express from "express";
 dotenv.config({ path: ".env"});
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT;
 
 app.get( "/", ( req, res ) => {
-	return res.send("Welcome to irrigation place");
+	return res.send("Welcome to irrigation server");
 });
 
 app.post("/data", (req, res) => {
-  return res.status(200).json({statusCode: 200, data: req.body});
-})
+  // save to a database;
+  return res.status(200).json({ statusCode: 200, data: req.body, message: "data logged successfully" });
+});
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
-});
+}); 
