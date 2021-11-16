6e6003f1f8d8a14d17aa37c27d3051d72b890797
Agbele Kolade <kolagbele@gmail.com>
2021-11-16 12:56:14 +0100

    app setup done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-// index file.
+import * as dotenv from "dotenv";
+import express from "express";
+
+dotenv.config({ path: ".env"});
+const app = express();
+
+const port = process.env.PORT;
+
+app.get( "/", ( req, res ) => {
+	return res.send("Welcome to irrigation place");
+});
+
+app.post("/data", (req, res) => {
+  return res.status(200).json({statusCode: 200, data: req.body});
+})
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
