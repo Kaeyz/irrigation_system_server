c8ce2d7db9d84f521471b5f5a239994d12519212
Agbele Kolade <kolagbele@gmail.com>
2021-11-21 19:15:51 +0100

    user docs added

--- src/docs/user.ts
+++ src/docs/user.ts
@@ -9,6 +9,24 @@
 /**
  * @swagger
  *
+ * /users/me:
+ *   get:
+ *     summary: Get Logged In User
+ *     tags: [User]
+ *     parameters:
+ *       - name: token
+ *         in: header
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ *
  * /users:
  *   post:
  *     tags: [User]
@@ -70,6 +88,123 @@
  *               lastName:
  *                 type: string
  *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success response
+ *       406:
+ *         description: Input validation error
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ *
+ * /users/login:
+ *   post:
+ *     tags: [User]
+ *     summary: User login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *              - email
+ *              - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success response
+ *       406:
+ *         description: Input validation error
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ *
+ * /users/forgot:
+ *   post:
+ *     tags: [User]
+ *     summary: User Forgot password 
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *              - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success response
+ *       406:
+ *         description: Input validation error
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ *
+ * /users/verify:
+ *   post:
+ *     tags: [User]
+ *     summary: User verify token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *              - token
+ *             properties:
+ *               token:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success response
+ *       406:
+ *         description: Input validation error
+ *       500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ *
+ * /users/reset-password:
+ *   post:
+ *     tags: [User]
+ *     summary: Reset User Password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *              - token
+ *              - password
+ *              - confirmPassword
+ *             properties:
+ *               token:
  *                 type: string
  *               password:
  *                 type: string
